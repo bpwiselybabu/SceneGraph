91143d6ac1f1cd8dd307df59a8372891e3e46a8d
bpwiselybabu <bpwiselybabu@wpi.edu>
2016-07-21 16:05:58 -0700

    * release mode compilation test

--- cmake_modules/FindASSIMP.cmake
+++ cmake_modules/FindASSIMP.cmake
@@ -12,7 +12,7 @@ FIND_PATH( ASSIMP_INCLUDE_DIR assimp/mesh.h
 )
 
 
-FIND_LIBRARY( ASSIMP_LIBRARY assimp assimp-vc130-mtd
+FIND_LIBRARY( ASSIMP_LIBRARY assimp assimp-vc130-mtd assimp-vc130-mt
   /usr/lib64
   /usr/lib
   /usr/local/lib
